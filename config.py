c9d2990e8d4e718ca649f20b7c521609b2098d2b
Jogor <93825861+Bebrus228@users.noreply.github.com>
2022-06-09 12:31:33 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-info = {
-	'author':'Jogor',
-	'version':'1.0',
-	'program_name':'file sorter',
-	'description':'this program sort files in this folder',
-	'gitHub_link': 'https://github.com/Bebrus228/'
-}
