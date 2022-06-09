e1feb094b28fcfd8589c656dd35715b4950166fa
Jogor <93825861+Bebrus228@users.noreply.github.com>
2022-06-09 12:26:24 +0300

    Add files via upload

---
+++ Compiled App/config.py
@@ -0,0 +1,7 @@
+info = {
+	'author':'Jogor',
+	'version':'beta 1.1',
+	'program_name':'file sorter',
+	'description':'this program sort files in this folder',
+	'gitHub_link': 'https://github.com/Bebrus228/'
+}
