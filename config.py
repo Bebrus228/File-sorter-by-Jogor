f16019ffa0e9adea7e7676d7d4d782b09936e29c
Jogor <93825861+Bebrus228@users.noreply.github.com>
2022-06-09 10:02:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 info = {
 	'author':'Jogor',
-	'version':'beta 1.0',
+	'version':'beta 1.1',
 	'program_name':'file sorter',
 	'description':'this program sort files in this folder',
 	'gitHub_link': 'https://github.com/Bebrus228/'
